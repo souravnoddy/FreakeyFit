f1f3934914a90cba27ae3cdf5eb34c58c69f7ace
sourav.purohit@gmail.com <sourav.purohit@tesco.com>
2021-04-02 17:52:10 +0530

    initial code base commit

---
+++ src/main/java/com/freaky/fit/FreakyFitApplication.java
@@ -0,0 +1,13 @@
+package com.freaky.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreakyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreakyFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freaky/fit/FreakyFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freaky.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreakyFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
